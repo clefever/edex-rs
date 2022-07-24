98170573df9e0ce8a2f1e4f4066041765211596c
Chris Lefever <linuxcl4@gmail.com>
2022-07-23 23:08:28 -0400

    Boot screen refactoring. Add title

--- src/components/boot_screen.rs
+++ src/components/boot_screen.rs
@@ -1,44 +1,57 @@
 use dioxus::{core::to_owned, prelude::*};
+use std::fs;
 use tokio::time::Duration;
 
 const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
 
+enum State {
+    Boot,
+    TitleTransition,
+    TitleScreen,
+}
+
 pub fn boot_screen(cx: Scope) -> Element {
+    let state = use_state(&cx, || State::Boot);
     let curr_line: &UseState<u32> = use_state(&cx, || 0);
     let lines = use_ref(&cx, || vec![]);
+    let class = use_state(&cx, || "");
 
     let all_lines = load_boot_log();
-    let total_lines = all_lines.len();
 
     // Time format example: "Fri Jul 15 2022 14:35:43 GMT-0400 (Eastern Daylight Time)"
 
     cx.spawn({
-        to_owned![curr_line, lines];
+        to_owned![class, curr_line, lines, state];
         async move {
-            let dur = timeout_from_line(*curr_line, total_lines);
-            tokio::time::sleep(Duration::from_millis(dur)).await;
-            if (*curr_line as usize) < total_lines {
-                let line = all_lines[*curr_line as usize].to_owned();
-                lines.write().push(line);
-                if *curr_line == 1 {
-                    lines.write().push(format!("eDEX-UI Kernel version {} boot at {}; root:xnu-1699.22.73~1/RELEASE_X86_64", VERSION.unwrap_or("unknown"), "FIXME"));
-                } else if *curr_line == 82 && is_arch_user() {
-                    lines.write().push(String::from("btw i use arch"));
+            match state.get() {
+                State::Boot => print_line(curr_line, state, lines, all_lines).await,
+                State::TitleTransition => {
+                    tokio::time::sleep(Duration::from_millis(400)).await;
+                    state.set(State::TitleScreen);
                 }
-                curr_line += 1;
+                State::TitleScreen => display_title(class).await,
             }
         }
     });
 
     cx.render(rsx!(
-        style { [include_str!("../assets/css/boot_screen.css")]}
-        section { id: "boot_screen",
-        lines.read().iter().map(|line| {
-            rsx!(
-                "{line}"
-                br {}
-            )
-        })
+        style { [include_str!("../assets/css/boot_screen.css")] }
+        section { class: "{class}", id: "boot_screen",
+        match state.get() {
+            State::Boot => {rsx!(
+                lines.read().iter().map(|line| {
+                    rsx!(
+                        "{line}"
+                        br {}
+                    )
+                })
+            )},
+            State::TitleTransition => rsx!(""),
+            State::TitleScreen => {
+                rsx!(h1 { "eDEX-rs "})
+            }
+        }
+
     }))
 }
 
@@ -55,11 +68,49 @@ fn timeout_from_line(line_num: u32, total_lines: usize) -> u64 {
     }
 }
 
+async fn print_line(
+    mut curr_line: UseState<u32>,
+    state: UseState<State>,
+    lines: UseRef<Vec<String>>,
+    all_lines: Vec<String>,
+) {
+    if (*curr_line as usize) < all_lines.len() {
+        let dur = timeout_from_line(*curr_line, all_lines.len());
+        tokio::time::sleep(Duration::from_millis(dur)).await;
+        if (*curr_line as usize) < all_lines.len() {
+            let line = all_lines[*curr_line as usize].to_owned();
+            lines.write().push(line);
+            if *curr_line == 1 {
+                lines.write().push(format!(
+                    "eDEX-UI Kernel version {} boot at {}; root:xnu-1699.22.73~1/RELEASE_X86_64",
+                    VERSION.unwrap_or("unknown"),
+                    "FIXME"
+                ));
+            } else if *curr_line == 82 && is_arch_user() {
+                lines.write().push(String::from("btw i use arch"));
+            }
+            curr_line += 1;
+        }
+    } else {
+        tokio::time::sleep(Duration::from_millis(300)).await;
+        state.set(State::TitleTransition);
+    }
+}
+
+async fn display_title(class: UseState<&str>) {
+    class.set("center");
+    // TODO: Some stuff that requires theme vars
+    tokio::time::sleep(Duration::from_millis(700)).await;
+}
+
 fn load_boot_log() -> Vec<String> {
     let layout = include_str!("../assets/misc/boot_log.txt");
     layout.split('\n').map(str::to_string).collect()
 }
 
 fn is_arch_user() -> bool {
-    false // TODO: Add arch detection logic
+    match fs::read_to_string("/etc/os-release") {
+        Ok(str) => str.contains("arch"),
+        Err(_) => false,
+    }
 }
